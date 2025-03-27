34db2d52250502929eb2a3f462e01894a5388bf3
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-03-27 19:06:12 +0100

    feat: fatto rotta index json

--- routers/posts.js
+++ routers/posts.js
@@ -9,26 +9,31 @@ const posts = require('../data/posts.js')
 router.get('/', (req,res)=>{
     res.json(posts)
 })
+
 // show
-router.get('/:id',(req,res) => {
-    res.json('dettagli del post')
+router.get('/:title',(req,res) => {
+    
 })
 
 // store
 router.post('/',(req,res) => {
     res.send('creazione nuovo post')
 })
+
 // update
-router.put('/:id',(req,res) => {
+router.put('/:title',(req,res) => {
     res.send('modifica di un post')
 })
+
 // modify
-router.patch('/:id',(req,res) => {
+router.patch('/:title',(req,res) => {
     res.send('modifica di parte di un post')
 })
+
 // destroy
-router.delete('/:id',(req,res) => {
+router.delete('/:title',(req,res) => {
     res.send('ccancellazione di un post post')
 })
 
+// esportazione router
 module.exports = router
